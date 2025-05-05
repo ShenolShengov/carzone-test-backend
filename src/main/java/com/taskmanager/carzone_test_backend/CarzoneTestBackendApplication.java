8f686304f2bce9498c097dac3f63ab4f309f7506
Shenol Shengov <shenolshengov41@gmail.com>
2025-05-05 22:32:50 +0300

    Initial commit

---
+++ src/main/java/com/taskmanager/carzone_test_backend/CarzoneTestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanager.carzone_test_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarzoneTestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarzoneTestBackendApplication.class, args);
+	}
+
+}
