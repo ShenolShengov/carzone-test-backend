ef8bc0242b481bb4b88d5f9dc0a47d01abce2057
hyuseinleshov <hyusein.leshov@gmail.com>
2025-05-08 23:41:31 +0300

    fix(model): add implementation for BaseEntity

--- src/main/java/com/taskmanager/carzone_test_backend/model/BaseEntity.java
+++ src/main/java/com/taskmanager/carzone_test_backend/model/BaseEntity.java
@@ -1,4 +1,17 @@
 package com.taskmanager.carzone_test_backend.model;
 
-public class BaseEntity {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
